3010df48ce98327929a1c1566204f9fbeed384a8
Pablo Tesone <tesonep@gmail.com>
2026-03-23 12:45:03 +0100

    Fixing rebenchs and scripts

--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -3,18 +3,10 @@
 set -x 
 
 rebench -D bloc.rebench
+rebench -D pharo.rebench
+rebench -D cormas.rebench
+rebench -D dataFrame.rebench
+rebench -D honeyGinger.rebench
+rebench -D microdown.rebench
 
-pushd pharo
-rebench -D ../pharo.rebench
-popd
-
-pushd veritas
-rebench -D ../cormas.rebench
-rebench -D ../dataFrame.rebench
-rebench -D ../honeyGinger.rebench
-rebench -D ../microdown.rebench
-popd
-
-pushd plots
-python script.py
-popd
+python ./scripts/generatePlots.py
